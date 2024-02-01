e499c18333bd0ad64d21b9be9e8619014437b451
Allan Schmidt Hansen <allansh@pm.me>
2024-02-01 16:22:24 +0100

    Added plugins for Neovim. Updated font for Alacritty on Windows

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -13,18 +13,54 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+        config = function()
+            vim.cmd[[colorscheme tokyonight-moon]]
+        end,
+    },
 
+    "tpope/vim-commentary", -- Commenting support -- Commenting support
+
+    -- Autopairing quotes, brackets...
+    {
+        'windwp/nvim-autopairs',
+        config = function()
+            require('nvim-autopairs').setup()
+        end,
+    },
+
+    {
+        'AndrewRadev/splitjoin.vim',
+        config = function()
+            vim.g.splitjoin_html_attributes_bracket_on_new_line = 1
+            vim.g.splitjoin_trailing_comma = 1
+            vim.g.splitjoin_php_method_chain_full = 1
+        end,
+    },
+
+    -- Status line
+    { import = 'user.plugins.lualine' },
+
+    -- Fuzzy finder
+    { import = 'user.plugins.telescope' },
+
+    -- File tree sidebar
+    { import = 'user.plugins.neo-tree' },
 }, {
   checker = {
     enabled = true,
     notify = false,
   },
   change_detection = {
-    notify = false,
+    notify = true,
 
   },
 
   install = {
---    colorscheme = { "tokyonight", "habamax" },
+    colorscheme = { "tokyonight", "habamax" },
   },
 })

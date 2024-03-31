9afc34273862193440cda64a3ac0700b55c087d0
Allan Schmidt Hansen <ash@helash.dev>
2024-03-31 23:59:43 +0200

    Moved gitconfig. Added intelephense to LSP.

--- nvim/lua/user/plugins/cmp.lua
+++ nvim/lua/user/plugins/cmp.lua
@@ -1,6 +1,7 @@
 return {
   'hrsh7th/nvim-cmp',
   event = "InsertEnter",
+  lazy_load = false,
   dependencies = {
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-nvim-lsp-signature-help',
--- nvim/lua/user/plugins/lspconfig.lua
+++ nvim/lua/user/plugins/lspconfig.lua
@@ -21,10 +21,42 @@ return {
     local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
     -- PHP
+    require('lspconfig').intelephense.setup({
+      commands = {
+        IntelephenseIndex = {
+          function()
+            vim.lsp.buf.execute_command({ command = 'intelephense.index.workspace' })
+          end,
+        },
+      },
+      on_attach = function(client, bufnr)
+        client.server_capabilities.documentFormattingProvider = false
+        client.server_capabilities.documentRangeFormattingProvider = false
+        -- if client.server_capabilities.inlayHintProvider then
+        --   vim.lsp.buf.inlay_hint(bufnr, true)
+        -- end
+      end,
+      capabilities = capabilities
+    })
+
     require('lspconfig').phpactor.setup({
       capabilities = capabilities,
-      on_attach = on_attach,
-
+      on_attach = function(client, bufnr)
+        client.server_capabilities.completionProvider = false
+        client.server_capabilities.hoverProvider = false
+        client.server_capabilities.implementationProvider = false
+        client.server_capabilities.referencesProvider = false
+        client.server_capabilities.renameProvider = false
+        client.server_capabilities.selectionRangeProvider = false
+        client.server_capabilities.signatureHelpProvider = false
+        client.server_capabilities.typeDefinitionProvider = false
+        client.server_capabilities.workspaceSymbolProvider = false
+        client.server_capabilities.definitionProvider = false
+        client.server_capabilities.documentHighlightProvider = false
+        client.server_capabilities.documentSymbolProvider = false
+        client.server_capabilities.documentFormattingProvider = false
+        client.server_capabilities.documentRangeFormattingProvider = false
+      end,
       init_options = {
         ["language_server_phpstan.enabled"] = false,
         ["language_server_psalm.enabled"] = false,

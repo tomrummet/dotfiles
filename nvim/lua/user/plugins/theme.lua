ec5053c2fdd011df9ee73c969a159cb181a43d22
Allan Schmidt Hansen <ash@helash.dev>
2024-05-18 22:58:36 +0200

    Changed neovim theme

--- nvim/lua/user/plugins/theme.lua
+++ nvim/lua/user/plugins/theme.lua
@@ -4,7 +4,7 @@ return {
     priority = 1000,
     config = function()
         require("tokyonight").setup({
-            style = "moon",
+            style = "night",
             styles = {
                 comments = { italic = true },
                 keywords = { italic = true },

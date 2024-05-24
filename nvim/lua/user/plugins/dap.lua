0241ce12a78358be8ec4bc9e959768d7a7a8ba1f
Allan Schmidt Hansen <ash@ipwsystems.dk>
2024-05-24 17:10:42 +0200

    DAP setup

--- nvim/lua/user/plugins/dap.lua
+++ nvim/lua/user/plugins/dap.lua
@@ -1,10 +1,38 @@
 return {
-    'jay-babu/mason-nvim-dap.nvim',
+    'mfussenegger/nvim-dap',
+
     dependencies = {
         "mason.nvim",
-        "rcarriga/nvim-dap-ui",
+        'jay-babu/mason-nvim-dap.nvim',
+        'rcarriga/nvim-dap-ui',
+        'nvim-neotest/nvim-nio'
     },
     cmd = { "DapInstall", "DapUninstall" },
+    config = function()
+        local dap = require('dap')
+        local dapui = require('dapui')
+
+        dap.listeners.before.attach.dapui_config = function()
+            dapui.open()
+        end
+        dap.listeners.before.launch.dapui_config = function()
+            dapui.open()
+        end
+        dap.listeners.before.event_terminated.dapui_config = function()
+            dapui.close()
+        end
+        dap.listeners.before.event_exited.dapui_config = function()
+            dapui.close()
+        end
+
+        vim.keymap.set('n', '<F5>', function() dap.continue() end)
+        vim.keymap.set('n', '<F10>', function() dap.step_over() end)
+        vim.keymap.set('n', '<F11>', function() dap.step_into() end)
+        vim.keymap.set('n', '<F12>', function() dap.step_out() end)
+        vim.keymap.set('n', '<Leader>db', function() dap.toggle_breakpoint() end)
+        vim.keymap.set('n', '<Leader>dB', function() dap.set_breakpoint() end)
+
+    end,
     opts = {
         -- Makes a best effort to setup the various debuggers with
         -- reasonable debug configurations

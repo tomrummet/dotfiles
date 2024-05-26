53273360836d1a066f77a404afedb6a24c9338fb
Allan Schmidt Hansen <ash@helash.dev>
2024-05-26 17:05:34 +0200

    Working on dap and xdebug

--- nvim/lua/user/plugins/dap.lua
+++ nvim/lua/user/plugins/dap.lua
@@ -1,16 +1,38 @@
 return {
-    'mfussenegger/nvim-dap',
-
+    "rcarriga/nvim-dap-ui",
     dependencies = {
-        "mason.nvim",
-        'jay-babu/mason-nvim-dap.nvim',
-        'rcarriga/nvim-dap-ui',
-        'nvim-neotest/nvim-nio'
+        "mfussenegger/nvim-dap",
+        "nvim-neotest/nvim-nio",
+        "nvim-telescope/telescope-dap.nvim"
     },
-    cmd = { "DapInstall", "DapUninstall" },
     config = function()
-        local dap = require('dap')
-        local dapui = require('dapui')
+        local dap, dapui = require("dap"), require("dapui")
+
+        require("dapui").setup()
+        require('telescope').load_extension('dap')
+
+        dap.adapters.php = {
+            type = "executable",
+            command = "node",
+            args = { os.getenv("HOME") .. "/vscode-php-debug/out/phpDebug.js" }
+        }
+
+        dap.configurations.php = {
+            {
+                type = "php",
+                request = "launch",
+                name = "Listen for Xdebug",
+                port = 9003
+            }
+        }
+        vim.keymap.set('n', '<F5>', function() dap.continue() end)
+        vim.keymap.set('n', '<F10>', function() dap.step_over() end)
+        vim.keymap.set('n', '<F11>', function() dap.step_into() end)
+        vim.keymap.set('n', '<F12>', function() dap.step_out() end)
+        vim.keymap.set('n', '<Leader>db', function() dap.toggle_breakpoint() end)
+        vim.keymap.set('n', '<Leader>dB', function() dap.set_breakpoint() end)
+
+        dapui.setup()
 
         dap.listeners.before.attach.dapui_config = function()
             dapui.open()
@@ -24,28 +46,5 @@ return {
         dap.listeners.before.event_exited.dapui_config = function()
             dapui.close()
         end
-
-        vim.keymap.set('n', '<F5>', function() dap.continue() end)
-        vim.keymap.set('n', '<F10>', function() dap.step_over() end)
-        vim.keymap.set('n', '<F11>', function() dap.step_into() end)
-        vim.keymap.set('n', '<F12>', function() dap.step_out() end)
-        vim.keymap.set('n', '<Leader>db', function() dap.toggle_breakpoint() end)
-        vim.keymap.set('n', '<Leader>dB', function() dap.set_breakpoint() end)
-
-    end,
-    opts = {
-        -- Makes a best effort to setup the various debuggers with
-        -- reasonable debug configurations
-        automatic_installation = true,
-
-        -- You can provide additional configuration to the handlers,
-        -- see mason-nvim-dap README for more information
-        handlers = {},
-
-        -- You'll need to check that you have the required things installed
-        -- online, please don't ask me how to install them :)
-        ensure_installed = {
-            -- Update this to ensure that you have the debuggers for the langs you want
-        },
-    },
+    end
 }

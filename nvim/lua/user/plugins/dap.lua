a17d55fe79200c722657a36bb8e5f589bca707c2
Allan Schmidt Hansen <ash@helash.dev>
2024-05-26 22:42:07 +0200

    Working on DAP + Xdebug

--- nvim/lua/user/plugins/dap.lua
+++ nvim/lua/user/plugins/dap.lua
@@ -1,50 +1,126 @@
 return {
-    "rcarriga/nvim-dap-ui",
+    "mfussenegger/nvim-dap",
+    recommended = true,
+    desc = "Debugging support. Requires language specific adapters to be configured. (see lang extras)",
+
     dependencies = {
-        "mfussenegger/nvim-dap",
-        "nvim-neotest/nvim-nio",
-        "nvim-telescope/telescope-dap.nvim"
+        -- fancy UI for the debugger
+        {
+            "rcarriga/nvim-dap-ui",
+            dependencies = { "nvim-neotest/nvim-nio" },
+            -- stylua: ignore
+            keys = {
+                { "<leader>du", function() require("dapui").toggle({ }) end, desc = "Dap UI" },
+                { "<leader>de", function() require("dapui").eval() end, desc = "Eval", mode = {"n", "v"} },
+            },
+            opts = {},
+            config = function(_, opts)
+                local dap = require("dap")
+                local dapui = require("dapui")
+                dapui.setup(opts)
+                dap.listeners.after.event_initialized["dapui_config"] = function()
+                    dapui.open({})
+                end
+                dap.listeners.before.event_terminated["dapui_config"] = function()
+                    dapui.close({})
+                end
+                dap.listeners.before.event_exited["dapui_config"] = function()
+                    dapui.close({})
+                end
+            end,
+        },
+
+        -- virtual text for the debugger
+        {
+            "theHamsta/nvim-dap-virtual-text",
+            opts = {},
+        },
+
+        -- mason.nvim integration
+        {
+            "jay-babu/mason-nvim-dap.nvim",
+            dependencies = "mason.nvim",
+            cmd = { "DapInstall", "DapUninstall" },
+            opts = {
+                -- Makes a best effort to setup the various debuggers with
+                -- reasonable debug configurations
+                automatic_installation = true,
+
+                -- You can provide additional configuration to the handlers,
+                -- see mason-nvim-dap README for more information
+                handlers = {
+                },
+
+                -- You'll need to check that you have the required things installed
+                -- online, please don't ask me how to install them :)
+                ensure_installed = {
+                    'php-debug-adapter',
+                    -- Update this to ensure that you have the debuggers for the langs you want
+                },
+            },
+        },
+
+        -- VsCode launch.json parser
+        {
+            "folke/neoconf.nvim",
+        },
     },
+
+    -- stylua: ignore
+    keys = {
+        { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = "Breakpoint Condition" },
+        { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
+        { "<leader>dc", function() require("dap").continue() end, desc = "Continue" },
+        { "<leader>da", function() require("dap").continue({ before = get_args }) end, desc = "Run with Args" },
+        { "<leader>dC", function() require("dap").run_to_cursor() end, desc = "Run to Cursor" },
+        { "<leader>dg", function() require("dap").goto_() end, desc = "Go to Line (No Execute)" },
+        { "<leader>di", function() require("dap").step_into() end, desc = "Step Into" },
+        { "<leader>dj", function() require("dap").down() end, desc = "Down" },
+        { "<leader>dk", function() require("dap").up() end, desc = "Up" },
+        { "<leader>dl", function() require("dap").run_last() end, desc = "Run Last" },
+        { "<leader>do", function() require("dap").step_out() end, desc = "Step Out" },
+        { "<leader>dO", function() require("dap").step_over() end, desc = "Step Over" },
+        { "<leader>dp", function() require("dap").pause() end, desc = "Pause" },
+        { "<leader>dr", function() require("dap").repl.toggle() end, desc = "Toggle REPL" },
+        { "<leader>ds", function() require("dap").session() end, desc = "Session" },
+        { "<leader>dt", function() require("dap").terminate() end, desc = "Terminate" },
+        { "<leader>dw", function() require("dap.ui.widgets").hover() end, desc = "Widgets" },
+    },
+
     config = function()
-        local dap, dapui = require("dap"), require("dapui")
-
-        require("dapui").setup()
-        require('telescope').load_extension('dap')
-
+        local dap = require("dap")
         dap.adapters.php = {
             type = "executable",
             command = "node",
-            args = { os.getenv("HOME") .. "/vscode-php-debug/out/phpDebug.js" }
+            args = {
+                vim.loop.os_homedir() .. "/.local/share/nvim/mason/packages/php-debug-adapter/extension/out/phpDebug.js",
+            },
         }
+        require("dap.ext.vscode").load_launchjs(vim.fn.getcwd() .. "/.vscode/launch.json", {})
 
-        dap.configurations.php = {
-            {
-                type = "php",
-                request = "launch",
-                name = "Listen for Xdebug",
-                port = 9003
-            }
-        }
-        vim.keymap.set('n', '<F5>', function() dap.continue() end)
-        vim.keymap.set('n', '<F10>', function() dap.step_over() end)
-        vim.keymap.set('n', '<F11>', function() dap.step_into() end)
-        vim.keymap.set('n', '<F12>', function() dap.step_out() end)
-        vim.keymap.set('n', '<Leader>db', function() dap.toggle_breakpoint() end)
-        vim.keymap.set('n', '<Leader>dB', function() dap.set_breakpoint() end)
+        -- local Config = require("lazyvim.config")
+        --
+        vim.api.nvim_set_hl(0, "DapStoppedLine", { default = true, link = "Visual" })
 
-        dapui.setup()
+        -- for name, sign in pairs(Config.icons.dap) do
+        --   sign = type(sign) == "table" and sign or { sign }
+        --   vim.fn.sign_define(
+        --     "Dap" .. name,
+        --     { text = sign[1], texthl = sign[2] or "DiagnosticInfo", linehl = sign[3], numhl = sign[3] }
+        --   )
+        -- end
 
-        dap.listeners.before.attach.dapui_config = function()
-            dapui.open()
+        -- setup dap config by VsCode launch.json file
+        local vscode = require("dap.ext.vscode")
+        local _filetypes = require("mason-nvim-dap.mappings.filetypes")
+        local filetypes = vim.tbl_deep_extend("force", _filetypes, {
+            ["node"] = { "javascriptreact", "typescriptreact", "typescript", "javascript" },
+            ["pwa-node"] = { "javascriptreact", "typescriptreact", "typescript", "javascript" },
+        })
+        local json = require("plenary.json")
+        vscode.json_decode = function(str)
+            return vim.json.decode(json.json_strip_comments(str))
         end
-        dap.listeners.before.launch.dapui_config = function()
-            dapui.open()
-        end
-        dap.listeners.before.event_terminated.dapui_config = function()
-            dapui.close()
-        end
-        dap.listeners.before.event_exited.dapui_config = function()
-            dapui.close()
-        end
-    end
+        vscode.load_launchjs(nil, filetypes)
+    end,
 }

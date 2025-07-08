9c83849806d6ab1345b23c33518e352108966753
Allan Schmidt Hansen <ash@helash.dev>
2025-07-08 16:28:01 +0200

    Removed sudo use from yay. Updated docker install to Arch

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -4,47 +4,15 @@ gum log --structured --time="DateTime" --level info "Installing Docker and Docke
 
 # Installs Docker and Docker Compose
 if [ ! "$RUNNING_DOTFILES" = 1 ]; then
-    sudo apt update
+    yay
 fi
 
 # Install dependencies
-sudo apt install -y \
-    ca-certificates \
-    curl
+yay -S --needed --noconfirm curl docker docker-compose
 
-IS_DEBIAN=$(uname -v | rg -c "Debian")
-
-# Set up repository
-if [ "$IS_DEBIAN" = "1" ]; then
-    if [ ! -f "/etc/apt/keyrings/docker.asc" ]; then
-        sudo install -m 0755 -d /etc/apt/keyrings
-        sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-        sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-        echo \
-            "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
-            $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-            sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    fi
-else
-    sudo apt install -y \
-        apt-transport-https \
-        software-properties-common
-
-    if [ ! -f "/usr/share/keyrings/docker-archive-keyring.gpg" ]; then
-        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-
-        echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    fi
-fi
-
-sudo apt update
-
-sudo apt install -y \
-    docker-ce \
-    docker-ce-cli \
-    docker-compose-plugin
+systemctl enable docker.service
 
 sudo groupadd docker
 sudo usermod -aG docker $USER
+newgrp docker
 

dea82e60832923ccdbb425b29c06b35eda6158c8
Allan Schmidt Hansen <ash@ipwsystems.dk>
2025-10-15 09:03:31 +0200

    Handled deprecation warning in lspconfig and fixed issue with bufferline and catppuccin

--- nvim/lua/user/plugins/lspconfig.lua
+++ nvim/lua/user/plugins/lspconfig.lua
@@ -19,57 +19,57 @@ return {
     local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
     -- PHP
-    -- require('lspconfig').intelephense.setup({
-    --   commands = {
-    --     IntelephenseIndex = {
-    --       function()
-    --         vim.lsp.buf.execute_command({ command = 'intelephense.index.workspace' })
-    --       end,
-    --     },
-    --   },
-    --   on_attach = function(client, bufnr)
-    --     client.server_capabilities.documentFormattingProvider = false
-    --     client.server_capabilities.documentRangeFormattingProvider = false
+    vim.lsp.config('intelephense',  {
+      commands = {
+        IntelephenseIndex = {
+          function()
+            vim.lsp.buf.execute_command({ command = 'intelephense.index.workspace' })
+          end,
+        },
+      },
+      on_attach = function(client, bufnr)
+        client.server_capabilities.documentFormattingProvider = false
+        client.server_capabilities.documentRangeFormattingProvider = false
 
-    --     -- if client.server_capabilities.inlayHintProvider then
-    --     --   vim.lsp.buf.inlay_hint(bufnr, true)
-    --     -- end
-    --   end,
-    --   capabilities = capabilities
-    -- })
+        -- if client.server_capabilities.inlayHintProvider then
+        --   vim.lsp.buf.inlay_hint(bufnr, true)
+        -- end
+      end,
+      capabilities = capabilities
+    })
 
-    -- require('lspconfig').phpactor.setup({
-    --   capabilities = capabilities,
-    --   on_attach = function(client, bufnr)
-    --     client.server_capabilities.completionProvider = false
-    --     client.server_capabilities.hoverProvider = false
-    --     client.server_capabilities.implementationProvider = false
-    --     client.server_capabilities.referencesProvider = false
-    --     client.server_capabilities.renameProvider = false
-    --     client.server_capabilities.selectionRangeProvider = false
-    --     client.server_capabilities.signatureHelpProvider = false
-    --     client.server_capabilities.typeDefinitionProvider = false
-    --     client.server_capabilities.workspaceSymbolProvider = false
-    --     client.server_capabilities.definitionProvider = false
-    --     client.server_capabilities.documentHighlightProvider = false
-    --     client.server_capabilities.documentSymbolProvider = false
-    --     client.server_capabilities.documentFormattingProvider = false
-    --     client.server_capabilities.documentRangeFormattingProvider = false
-    --   end,
-    --   init_options = {
-    --     ["language_server_phpstan.enabled"] = false,
-    --     ["language_server_psalm.enabled"] = false,
-    --     ["php_code_sniffer.enabled"] = false,
-    --     ["language_server_php_cs_fixer.enabled"] = false,
-    --     ["language_server.diagnostic_outsource"] = false,
-    --   },
-    --   handlers = {
-    --     ['textDocument/publishDiagnostics'] = function() end
-    --   }
-    -- })
+    vim.lsp.config('phpactor', {
+      capabilities = capabilities,
+      on_attach = function(client, bufnr)
+        client.server_capabilities.completionProvider = false
+        client.server_capabilities.hoverProvider = false
+        client.server_capabilities.implementationProvider = false
+        client.server_capabilities.referencesProvider = false
+        client.server_capabilities.renameProvider = false
+        client.server_capabilities.selectionRangeProvider = false
+        client.server_capabilities.signatureHelpProvider = false
+        client.server_capabilities.typeDefinitionProvider = false
+        client.server_capabilities.workspaceSymbolProvider = false
+        client.server_capabilities.definitionProvider = false
+        client.server_capabilities.documentHighlightProvider = false
+        client.server_capabilities.documentSymbolProvider = false
+        client.server_capabilities.documentFormattingProvider = false
+        client.server_capabilities.documentRangeFormattingProvider = false
+      end,
+      init_options = {
+        ["language_server_phpstan.enabled"] = false,
+        ["language_server_psalm.enabled"] = false,
+        ["php_code_sniffer.enabled"] = false,
+        ["language_server_php_cs_fixer.enabled"] = false,
+        ["language_server.diagnostic_outsource"] = false,
+      },
+      handlers = {
+        ['textDocument/publishDiagnostics'] = function() end
+      }
+    })
 
     -- Vue, JavaScript, TypeScript
-    require('lspconfig').volar.setup({
+    vim.lsp.config('volar', {
       on_attach = function(client, bufnr)
         client.server_capabilities.documentFormattingProvider = false
         client.server_capabilities.documentRangeFormattingProvider = false
@@ -82,7 +82,7 @@ return {
     })
 
     -- JSON
-    require('lspconfig').jsonls.setup({
+    vim.lsp.config('jsonls', {
       capabilities = capabilities,
       settings = {
         json = {
@@ -95,7 +95,7 @@ return {
     mason_lspconfig.setup {
        ensure_installed = { 'pyright' }
     }
-    require('lspconfig').pyright.setup({
+    vim.lsp.config('pyright', {
         capabilities = capabilities,
     })
 
